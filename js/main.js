d4d9d297cee97725029378d2fe0154bc45f59341
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-04-14 22:14:33 +0200

    Menu btn and nav links functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+
+    const menuBtn = header.querySelector('.menu-btn');
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('open');
+
+        const navLinks = header.querySelector('.nav-links');
+        navLinks.classList.toggle('open');
+    });
     
-    const controlBtns = document.querySelectorAll('.controls button');
+    const controlBtns = header.querySelectorAll('.controls button');
     
     controlBtns.forEach(btn => {
         btn.addEventListener('click', () => {
