8acd886916a2bc0fdda68fabb74d11299e55cac9
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-04-19 20:32:47 +0200

    Header BGs functionality on mobile

--- js/main.js
+++ js/main.js
@@ -11,25 +11,30 @@ window.addEventListener('DOMContentLoaded', () => {
     
     const controlBtns = document.querySelectorAll('.controls button');
     // const textSlides = document.querySelector('[data-slides]');
+    const textSlides = document.querySelector('[data-slides]');
+    const imgSlides = [
+        'url("./images/mobile-image-hero-1.jpg")',
+        'url("./images/mobile-image-hero-2.jpg")',
+        'url("./images/mobile-image-hero-3.jpg")'
+    ];
+
+    const header = document.querySelector('header');
+    header.style.backgroundImage = imgSlides[0];
 
     controlBtns.forEach(btn => {
         btn.addEventListener('click', () => {
             const step = btn.dataset.control === 'left' ? -1 : 1;
-            // console.log(step)
-            const textSlides = document.querySelector('[data-slides]');
-
+            
             const active = textSlides.querySelector('[data-active]');
-            console.log([...textSlides.children].indexOf(active))
 
             let newIndex = [...textSlides.children].indexOf(active) + step;
-            console.log(newIndex)
             
             if (newIndex < 0) newIndex = textSlides.children.length - 1;
             if (newIndex >= textSlides.children.length) newIndex = 0;
 
+            header.style.backgroundImage = imgSlides[newIndex];
             textSlides.children[newIndex].dataset.active = true;
-            delete active.dataset.active;
-            
+            delete active.dataset.active;            
         });
     });
 
