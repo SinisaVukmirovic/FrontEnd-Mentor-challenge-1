f2fc20995d89b15d7d8d4bd4504e20b98bd67f84
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-04-19 20:44:50 +0200

    Header BGs for desktop

--- js/main.js
+++ js/main.js
@@ -12,14 +12,22 @@ window.addEventListener('DOMContentLoaded', () => {
     const controlBtns = document.querySelectorAll('.controls button');
     // const textSlides = document.querySelector('[data-slides]');
     const textSlides = document.querySelector('[data-slides]');
-    const imgSlides = [
+    const mobileImgSlides = [
         'url("./images/mobile-image-hero-1.jpg")',
         'url("./images/mobile-image-hero-2.jpg")',
         'url("./images/mobile-image-hero-3.jpg")'
     ];
+    const desktopImgSlides = [
+        'url("./images/desktop-image-hero-1.jpg")',
+        'url("./images/desktop-image-hero-2.jpg")',
+        'url("./images/desktop-image-hero-3.jpg")'
+    ];
 
     const header = document.querySelector('header');
-    header.style.backgroundImage = imgSlides[0];
+    header.style.backgroundImage = mobileImgSlides[0];
+    if (window.innerWidth > 1000) {
+        header.style.backgroundImage = desktopImgSlides[0];
+    }
 
     controlBtns.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -32,11 +40,20 @@ window.addEventListener('DOMContentLoaded', () => {
             if (newIndex < 0) newIndex = textSlides.children.length - 1;
             if (newIndex >= textSlides.children.length) newIndex = 0;
 
-            header.style.backgroundImage = imgSlides[newIndex];
+            header.style.backgroundImage = mobileImgSlides[newIndex];
+            if (window.innerWidth > 1000) {
+                header.style.backgroundImage = desktopImgSlides[newIndex];
+            }
+
             textSlides.children[newIndex].dataset.active = true;
             delete active.dataset.active;            
         });
     });
+
+    if (window.innerWidth > 1000) {
+
+        console.log('DESKTOP');
+    }
 
 
 
