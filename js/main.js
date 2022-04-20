180ff4199f3ffad1adc91f98a45147a839c0e1bc
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-04-20 20:57:10 +0200

    Put Img and text slides in obj

--- js/main.js
+++ js/main.js
@@ -12,21 +12,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const controlBtns = document.querySelectorAll('.controls button');
     // const textSlides = document.querySelector('[data-slides]');
     const textSlides = document.querySelector('[data-slides]');
-    const mobileImgSlides = [
-        'url("./images/mobile-image-hero-1.jpg")',
-        'url("./images/mobile-image-hero-2.jpg")',
-        'url("./images/mobile-image-hero-3.jpg")'
-    ];
-    const desktopImgSlides = [
-        'url("./images/desktop-image-hero-1.jpg")',
-        'url("./images/desktop-image-hero-2.jpg")',
-        'url("./images/desktop-image-hero-3.jpg")'
-    ];
+    const imgSlides = {
+        mobile: [
+            'url("./images/mobile-image-hero-1.jpg")',
+            'url("./images/mobile-image-hero-2.jpg")',
+            'url("./images/mobile-image-hero-3.jpg")'
+        ],
+        desktop: [
+            'url("./images/desktop-image-hero-1.jpg")',
+            'url("./images/desktop-image-hero-2.jpg")',
+            'url("./images/desktop-image-hero-3.jpg")'
+        ]
+    }
 
     const header = document.querySelector('header');
-    header.style.backgroundImage = mobileImgSlides[0];
+    header.style.backgroundImage = imgSlides.mobile[0];
+
     if (window.innerWidth > 1000) {
-        header.style.backgroundImage = desktopImgSlides[0];
+        header.style.backgroundImage = imgSlides.desktop[0];
     }
 
     controlBtns.forEach(btn => {
@@ -40,9 +43,10 @@ window.addEventListener('DOMContentLoaded', () => {
             if (newIndex < 0) newIndex = textSlides.children.length - 1;
             if (newIndex >= textSlides.children.length) newIndex = 0;
 
-            header.style.backgroundImage = mobileImgSlides[newIndex];
+            header.style.backgroundImage = imgSlides.mobile[newIndex];
+
             if (window.innerWidth > 1000) {
-                header.style.backgroundImage = desktopImgSlides[newIndex];
+                header.style.backgroundImage = imgSlides.desktop[newIndex];
             }
 
             textSlides.children[newIndex].dataset.active = true;
